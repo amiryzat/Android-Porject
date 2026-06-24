bc5a55e102de1ad45a62c5dfddcc60e9e7c19879
amiryzat <amiryzat@gmail.com>
2026-06-24 08:44:29 +0800

    fix chat individual behaviour to task behave

--- app/src/main/java/util/ChatIdHelper.kt
+++ app/src/main/java/util/ChatIdHelper.kt
@@ -4,6 +4,6 @@ object ChatIdHelper {
 
     fun buildChatId(taskId: String, userA: String, userB: String): String {
         val users = listOf(userA, userB).sorted()
-        return "chat_${users[0]}_${users[1]}"
+        return "${taskId}_${users[0]}_${users[1]}"
     }
 }
